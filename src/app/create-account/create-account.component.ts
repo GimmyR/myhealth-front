0e04348d9b2cf2375b4515a60d1fd1aebcf2830f
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2022-12-07 16:35:15 +0300

    affichage de message et ajout d'animations pour le composant 'create-account'

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -16,6 +16,10 @@ export class CreateAccountComponent implements OnInit {
   password!: FormControl;
   confirm!: FormControl;
 
+  alert!: boolean;
+  alertMessage!: string;
+  isLoading!: boolean;
+
   constructor(private http: HttpClient, private router: Router) {}
 
   ngOnInit(): void {
@@ -24,12 +28,19 @@ export class CreateAccountComponent implements OnInit {
     this.email = new FormControl(null);
     this.password = new FormControl(null);
     this.confirm = new FormControl(null);
+
+    this.alert = false;
+    this.alertMessage = "";
+    this.isLoading = false;
   }
 
-  createAccount() {
-    if(this.password.value != this.confirm.value)
-      console.log("Les 2 mots de passe ne correspondent pas !");
-    else {
+  createAccount(e: Event) {
+    e.preventDefault();
+    if(this.password.value != this.confirm.value) {
+      this.alertMessage = "Les 2 mots de passe ne correspondent pas !";
+      this.alert = true;
+    } else {
+      this.isLoading = true;
       let body = {
         firstname: this.firstname.value,
         lastname: this.lastname.value,
@@ -39,11 +50,19 @@ export class CreateAccountComponent implements OnInit {
 
       this.http.post("http://localhost:8000/api/create-account", body, options)
         .subscribe((response: any) => {
+          this.isLoading = false;
           if(response.status == 0)
             this.router.navigateByUrl("");
-          else console.log(response);
+          else {
+            this.alertMessage = response.message;
+            this.alert = true;
+          }
         });
     }
+  }
+
+  closeAlert(): void {
+    this.alert = false;
   }
 
 }
